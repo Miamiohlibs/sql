d000ef7f5ca2c75b1d40d354aa4ccee272aa943f
Craig Boman <craig.boman@gmail.com>
2017-07-17 15:22:56 -0400

    tests

---
+++ fund-specific.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+*
+FROM
+sierra_view.fund AS f
+WHERE
+f.fund_code = 'sci'
+--continue to group all of the fund codes based on Karen's groupings in Google drive
+	
+;
--- fund-test.sql
+++ fund-test.sql
@@ -3,13 +3,24 @@
 FROM
 sierra_view.order_record AS o
 
-/*
+
+JOIN
+sierra_view.order_record_paid AS orp
+ON
+o.id = orp.order_record_id
+
+
 JOIN
 sierra_view.record_metadata AS m
 ON
 o.id = m.id
-*/
+
+JOIN
+sierra_view.order_record_received AS orr
+ON
+o.id = orr.item_record_metadata_id
+
 
 WHERE
-o.record_id = 476741893381
+m.record_num = 654887
 ;
