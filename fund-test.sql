796bdf27a92f026fd68e55acc7148da57f7e70ab
craigboman <craig.boman@gmail.com>
2017-09-21 15:02:36 -0400

    order tests

--- fund-test.sql
+++ fund-test.sql
@@ -1,5 +1,6 @@
 ﻿SELECT
-*
+count(*)
+ 
 FROM
 sierra_view.order_record AS o
 
@@ -7,21 +8,33 @@ sierra_view.order_record AS o
 JOIN
 sierra_view.order_record_paid AS orp
 ON
-o.id = orp.order_record_id
+o.record_id = orp.order_record_id
 
 
 JOIN
 sierra_view.record_metadata AS m
 ON
-o.id = m.id
+o.record_id = m.id
 
-/*
 JOIN
-sierra_view.order_record_received AS orr
+sierra_view.order_record_cmf AS f
 ON
-o.id = orr.item_record_metadata_id
-*/
+o.record_id = f.order_record_id
 
-WHERE
-m.record_num = 654887
+
+JOIN
+sierra_view.fund_master AS fm
+ON
+f.id = fm.id
+
+JOIN
+sierra_view.fund AS t
+ON
+fm.code = t.fund_code
+
+--LIMIT 100
+--WHERE
+
+--m.record_num = 654887
+--f.fund_code = '00993'--insert any local fund code here
 ;
---
+++ order record test-ray.sql
@@ -0,0 +1,22 @@
+﻿SELECT
+r.record_type_code || r.record_num || 'a' as record_num
+
+-- get the generic record metadata for the order record
+FROM
+sierra_view.record_metadata as r
+
+-- get the multi-field data for the order record
+JOIN
+sierra_view.order_record_cmf as f
+ON
+  f.order_record_id = r.id
+
+WHERE
+-- limiting to a range of dates for example
+r.creation_date_gmt >= '2017-01-01'
+and r.creation_date_gmt < '2018-01-01'
+and f.fund_code = '01208'
+
+GROUP BY
+r.record_type_code,
+r.record_num
