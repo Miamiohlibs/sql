baf3cf8b5bd3308794871e718bbbc641471a5740
craigboman <craig.boman@gmail.com>
2017-11-08 09:08:27 -0500

    final push

--- repeating245b.sql
+++ repeating245b.sql
@@ -1,14 +1,16 @@
-﻿select
---count(*)
-*
-from
+﻿select 'b'||record_num||'a'
+FROM
 sierra_view.subfield_view AS v
+
 WHERE
 v.record_type_code = 'b' 
+
 AND 
 v.marc_tag = '245' 
+
 AND
 v.tag = 'b'
-AND
-v.occ_num > 0
-LIMIT 1000
+group by v.record_num
+having count(v.record_num) > 1 
+ORDER BY v.record_num ASC
+limit 1000;
