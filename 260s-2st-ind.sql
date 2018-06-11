e778d792d4ced1b11c686d01df4936adb792029c
craigboman <craig.boman@gmail.com>
2018-06-11 14:43:37 -0400

    260s

---
+++ 260s-2st-ind.sql
@@ -0,0 +1,18 @@
+﻿SELECT
+count(*)
+FROM
+sierra_view.varfield AS v
+WHERE
+--do not limit by varfield type code unless it corresponds to 
+(v.marc_tag = '260'
+
+)
+AND
+(v.marc_ind2 = '0'
+OR
+v.marc_ind2 = '1' --null is also zero; must exclude nulls
+)
+limit 100
+;
+
+--add bib rec nums and delete cleanup 2nd ind's to show null or empty
