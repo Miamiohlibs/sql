2aacc661dd3eebb203a677e0c571c54b59dcbcbd
craigboman <craig.boman@gmail.com>
2018-06-11 14:21:23 -0400

    7xx

---
+++ 7xx-cleanup-2nd-ind.sql
@@ -0,0 +1,22 @@
+﻿SELECT
+count(*)
+FROM
+sierra_view.varfield AS v
+WHERE
+--do not limit by varfield type code unless it corresponds to 
+(v.marc_tag = '700'
+OR
+v.marc_tag = '710'
+OR
+v.marc_tag = '711'
+
+)
+AND
+v.marc_ind2 != ''
+AND
+v.marc_ind2 != '2'
+
+limit 100
+;
+
+--add bib rec nums and delete cleanup 2nd ind's to show null or empty
