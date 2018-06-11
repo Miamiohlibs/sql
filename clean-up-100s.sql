ff78a99d0980ce6d68207f2b6aa3493b28fb1ea8
craigboman <craig.boman@gmail.com>
2018-06-11 14:07:13 -0400

    sql cleanups

--- clean-up-100s.sql
+++ clean-up-100s.sql
@@ -1,12 +1,19 @@
 ﻿SELECT
-*
+count(*)
 FROM
 sierra_view.varfield AS v
 WHERE
-v.varfield_type_code = 'b'
-AND
-v.marc_tag = '100'
+--do not limit by varfield type code unless it corresponds to 
+(v.marc_tag = '100'
+OR
+v.marc_tag = '110'
+OR
+v.marc_tag = '111'
+
+)
 AND
 v.marc_ind2 != ''
 limit 100
 ;
+
+--add bib rec nums and delete cleanup 2nd ind's to show null or empty
