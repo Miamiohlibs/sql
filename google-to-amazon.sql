8ee2c2f4cea03866df520788e226b339015fa01b
craigboman <craig.boman@gmail.com>
2018-06-12 16:19:34 -0400

    amazon query

--- google-to-amazon.sql
+++ google-to-amazon.sql
@@ -1,13 +1,26 @@
-﻿--purpose of this query will be to take in an ISBN and 
+﻿--purpose of this query will be to take in an ISBN or title and return vendor code, etc
 SELECT
-*
+* --vendor, isbn, title, 
 FROM
 sierra_view.order_view AS o
 JOIN
 sierra_view.record_metadata AS m
 ON
 m.id = o.id
+JOIN
+sierra_view.varfield AS v
+ON
+v.record_id = o.record_id
+
+
+--doesn't return any indexed entries looking of ibsns
+-- JOIN
+-- sierra_view.phrase_entry AS p
+-- ON
+-- o.id = p.record_id 
+
 WHERE
+v.varfield_type_code = 
 o.record_creation_date_gmt IS NOT NULL
 ORDER BY o.record_creation_date_gmt DESC
 LIMIT 100;
