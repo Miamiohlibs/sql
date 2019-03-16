0d8ec550cd1ba3113ab27ed7f6dce55fa98a3224
craigboman <craig.boman@gmail.com>
2019-03-16 10:18:59 -0400

    updates

---
+++ OCLC holdings.sql
@@ -0,0 +1,7 @@
+﻿/*the goal of this code is to export our local list of OCLC numbers and push those into the Worldcat holdings API
+exclude holdings for ebooks, regional, and leisure collections*/
+
+SELECT
+*
+FROM
+WHERE
