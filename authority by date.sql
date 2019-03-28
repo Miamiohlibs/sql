2ad8cfbbe64f39c0a03281f1329735ec48012bb1
craigboman <craig.boman@gmail.com>
2019-03-28 16:06:47 -0400

    scoping authorities by date; churchill troubleshooting

---
+++ authority by date.sql
@@ -0,0 +1,18 @@
+﻿SELECT
+--r.record_num
+--*
+r.creation_date_gmt::date,
+count(r.creation_date_gmt::date)
+FROM
+sierra_view.record_metadata AS r
+WHERE
+r.record_type_code = 'i'
+GROUP BY
+r.creation_date_gmt::date
+
+
+ORDER BY r.creation_date_gmt::date DESC
+
+limit 356
+
+
---
+++ churchill_dups.sql
@@ -0,0 +1,19 @@
+﻿SELECT
+--m.creation_date_gmt
+*
+FROM
+sierra_view.item_record_property AS p
+JOIN
+sierra_view.item_record AS i
+ON
+p.item_record_id = i.record_id
+JOIN
+sierra_view.record_metadata AS m
+ON
+i.record_id = m.id
+WHERE
+p.barcode = '35054035185145'
+AND
+i.checkout_total > 0
+
+ORDER by m.creation_date_gmt
