71cad62c0f49f393cfa6cb32c4ee9b5c644d4530
craigboman <craig.boman@gmail.com>
2019-05-13 11:56:53 -0400

    deleted records visualization

---
+++ deleted_records_over_time.sql
@@ -0,0 +1,20 @@
+﻿SELECT
+m.deletion_date_gmt,
+-- *
+count(*)
+
+
+FROM
+sierra_view.record_metadata AS m
+WHERE
+m.record_type_code = 'b'
+AND
+m.campus_code = ''
+AND
+m.deletion_date_gmt IS NOT NULL
+
+GROUP BY m.deletion_date_gmt
+ORDER BY m.deletion_date_gmt DESC
+
+LIMIT 365
+
